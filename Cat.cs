0564b964aa76f5e276161ba72614d95ba273383d
339713067 <339713067@2260I-L29-023>
2026-01-08 09:40:12 -0500

    not done

---
+++ Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Game_2026
+{
+    internal class Cat
+    {
+        //private List<Texture2D> _Textures;
+        //private Vector2 _speed;
+        //private Rectangle _location;
+        //private int _textureIndex;
+      
+        //private float _anamationspeed;
+        //private float _seconds;
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace Final_Game_2026
 {
@@ -11,17 +13,30 @@ namespace Final_Game_2026
         private SpriteBatch _spriteBatch;
         Texture2D rectangleTexture;
 
+
         Rectangle player;
         Vector2 playerPosition;
         Vector2 speed;
+        Vector2 square1speed = new Vector2(2, 0);
+        Random generator;
+        List<Rectangle> square1rectangle;
+        List<Texture2D> square1textures;
+        Random random = new Random();
+        Rectangle window;
+        Texture2D square1texture;
+        float respawntime;
+        float seconds;
+
 
         List<Rectangle> platforms;
+
+
 
         KeyboardState keyboardState;
 
         float gravity = 0.3f;
         float jumpSpeed = 8f;
-        bool onGround = false;
+        bool onGround = false;  
 
         public Game1()
         {
@@ -33,6 +48,12 @@ namespace Final_Game_2026
 
         protected override void Initialize()
         {
+            generator = new Random();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            square1rectangle = new List<Rectangle>();
+            square1textures = new List<Texture2D>();
+
+            respawntime = 2f;
             speed = Vector2.Zero;
 
             playerPosition = new Vector2(10, 10);
@@ -50,6 +71,8 @@ namespace Final_Game_2026
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             rectangleTexture = Content.Load<Texture2D>("images/bluesquare");
+            square1texture = Content.Load<Texture2D>("Images/redsquare");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -59,8 +82,27 @@ namespace Final_Game_2026
             keyboardState = Keyboard.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-           
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (seconds >= respawntime)
+            {
+                for (int i = 1; i <= 5; i++)
+                    square1rectangle.Add(new Rectangle(
+                         generator.Next(window.Width, window.Width + 150),
+                         generator.Next(0, 130),
+                         50,
+                         50
+                         ));
+            }
+            Rectangle temp;
+            for (int i = 0; i < square1rectangle.Count; i++)
+            {
+                temp = square1rectangle[i];
+                temp.X += (int)square1speed.X;
+                square1rectangle[i] = temp;
+            }
+
             speed.Y += gravity;
+
 
             
             if (keyboardState.IsKeyDown(Keys.Space) && onGround)
@@ -68,8 +110,10 @@ namespace Final_Game_2026
                 speed.Y = -jumpSpeed;
                 onGround = false;
             }
+
             playerPosition.Y += speed.Y;
             player.Location = playerPosition.ToPoint();
+            onGround = false;
             foreach (Rectangle platform in platforms)
             {
                 if (player.Intersects(platform))
@@ -96,8 +140,12 @@ namespace Final_Game_2026
 
      
             _spriteBatch.Draw(rectangleTexture, player, Color.Red);
+            for (int i = 0; i < square1rectangle.Count; i++)
+            {
+                _spriteBatch.Draw(square1texture, square1rectangle[i], Color.White);
+            }
 
-           
+
             foreach (Rectangle platform in platforms)
             {
                 _spriteBatch.Draw(rectangleTexture, platform, Color.Black);
